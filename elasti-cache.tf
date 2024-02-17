c8276daa5adca80768bf3762a5dc187b3bfd9ebf
23-Pravallika <learngain23@gmail.com>
2024-02-17 22:22:59 +0530

    AutoPush

--- elasti-cache.tf
+++ elasti-cache.tf
@@ -1,11 +1,11 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "robo-${var.ENV}-redis"
   engine               = "redis"
-  node_type            = "cache.t3.medium"
-  num_cache_nodes      = 1
+  node_type            = var.REDIS_NODE_TYPE
+  num_cache_nodes      = var.REDIS_NODE_COUNT
   parameter_group_name = aws_elasticache_parameter_group.pg.name
-  engine_version       = "6.x"
-  port                 = 6379
+  engine_version       = var.REDIS_ENGINE_VERSION
+  port                 = var.REDIS_PORT
   security_group_ids   = [aws_security_group.allow-redis.id]
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
 }
@@ -13,7 +13,7 @@ resource "aws_elasticache_cluster" "redis" {
 # Creates Parameter group
 resource "aws_elasticache_parameter_group" "pg" {
   name   = "robo-${var.ENV}-redis-pg"
-  family = "redis6.x"
+  family = "redis${var.REDIS_ENGINE_VERSION}"
 }
 
 #Creates Subnet Group.
--- vars.tf
+++ vars.tf
@@ -1,2 +1,13 @@
 variable "ENV" {}
 
+# Elasticcache/Redis Variables 
+variable "REDIS_NODE_TYPE" {}
+variable "REDIS_ENGINE_VERSION" {}
+variable "REDIS_PORT" {}
+variable "REDIS_NODE_COUNT" {}
+
+
+
+
+
+
